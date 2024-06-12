a90c3789182a6cfeb62496d14acbcc25489f0cce
quan-le <31929073+quan-le@users.noreply.github.com>
2024-06-12 20:24:30 +0700

    Test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,10 @@
-module com.hcmiu.battleship {
+module com.hcmiu.tsweeper {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
 
-    opens com.hcmiu.battleship to javafx.fxml;
-    exports com.hcmiu.battleship;
+    opens com.hcmiu.tsweeper to javafx.fxml;
+    exports com.hcmiu.tsweeper;
 }
